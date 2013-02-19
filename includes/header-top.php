b690e3830f5172db1d4dcbbc3834a6d2e7d36d58
CGasberg <christian@cgasberg.dk>
2013-02-19 13:11:44 +0100

    created header-top

---
+++ includes/header-top.php
@@ -0,0 +1,18 @@
+
+<!doctype html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+<title>Home</title>
+
+<meta name="description" content="" />
+
+<link href="<?php echo $ROOT.'css/screen.css'; ?>" media="all" rel="stylesheet"/>
+
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="<?php echo $ROOT.'js/js.js'; ?>"></script>
